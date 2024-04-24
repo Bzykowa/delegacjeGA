50eae3490897bd9c5cb5106461cbed784ef5715a
Karolina Bąk <44176610+Bzykowa@users.noreply.github.com>
2024-04-24 14:57:26 +0200

    Update Prices.java

--- main/models/Prices.java
+++ main/models/Prices.java
@@ -2,9 +2,9 @@ package main.models;
 
 public final class Prices {
     public static final double perKilometre = 1.15;
-    public static final int perDayPL = 30;
-    public static final double perMealPL = 7.5;
-    public static final int perNightPL = 45;
+    public static final int perDayPL = 45;
+    public static final double perMealPL = 11.25;
+    public static final int perNightPL = 67.5;
     public static final int perDayDE = 49;
     public static final double perMealDE = 7.35;
     public static final double perNightDE = 42.5;
